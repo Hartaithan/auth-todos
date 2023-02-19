cc6042c0cdfdc32e1e259eac6ec1dc2f65fa5712
hartaithan <hartaithan@gmail.com>
2023-02-19 15:21:45 +0900

    add header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { TodoService } from 'src/app/services/todo/todo.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isLoading: boolean = true;
+  count: number | null = null;
+
+  constructor(private service: TodoService) {}
+
+  ngOnInit() {
+    this.service.getTodos().subscribe((todos) => {
+      this.isLoading = false;
+      this.count = todos.length;
+    });
+  }
+}
