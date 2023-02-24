52c60fdcecea129e5892e8b38d522373d5746428
hartaithan <hartaithan@gmail.com>
2023-02-24 16:13:02 +0900

    add auth guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,14 @@ import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { MainComponent } from './pages/main/main.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import {
+  canActivate,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
+
+const unAuthToSignIn = () => redirectUnauthorizedTo(['sign-in']);
+const authToMain = () => redirectLoggedInTo(['']);
 
 const routes: Routes = [
   {
@@ -11,11 +19,13 @@ const routes: Routes = [
     pathMatch: 'full',
     component: MainLayoutComponent,
     children: [{ path: '', component: MainComponent }],
+    ...canActivate(unAuthToSignIn),
   },
   {
     path: 'sign-in',
     component: AuthLayoutComponent,
     children: [{ path: '', component: SignInComponent }],
+    ...canActivate(authToMain),
   },
 ];
 
