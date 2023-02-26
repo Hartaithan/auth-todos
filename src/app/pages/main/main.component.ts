cb20e891a0443cd7718ae829088bfbebedc8d7c5
Hartaithan <hartaithan@gmail.com>
2023-02-26 15:01:27 +0900

    add temp layout for todo list

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ITodo } from 'src/app/models/todo.model';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { TodoService } from 'src/app/services/todo/todo.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {}
+export class MainComponent implements OnInit, OnDestroy {
+  isLoading = true;
+  todos: ITodo[] = [];
+  todosSub: Subscription | null = null;
+
+  constructor(private todo: TodoService, public auth: AuthService) {}
+
+  ngOnInit() {
+    this.todosSub = this.todo.getTodos().subscribe((todos) => {
+      this.isLoading = false;
+      this.todos = todos;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.todosSub) {
+      this.todosSub.unsubscribe();
+    }
+  }
+}
